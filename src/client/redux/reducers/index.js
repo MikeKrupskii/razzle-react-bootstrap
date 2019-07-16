55db08f7c06be486c46fa7d9389c4e78f9aa26a4
MikeK35 <mikekrupskii35@gmail.com>
2019-07-16 13:12:48 +0200

    changed env

--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import userReducers from './user.reducers'
-import taskReducers from './task.reducers'
 
 const rootReducer = combineReducers({
   form: formReducer,
-  userReducers,
-  taskReducers
 })
 
 export default rootReducer
